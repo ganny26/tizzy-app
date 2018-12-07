da341042c6dee229b056c15d02dbf81bda6f3e48
Selvaganesh <selvaganeshrajam@gmail.com>
2018-12-07 19:19:09 +0530

    gzip implemented 👍

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './Components/Home';
-
+import Stories from './Components/Stories';
 
 function App() {
   return (
     <div>
-      <Home name="Selvaganesh"/>
+      <Home name="Your are rendering gziped files"/>
+      <Stories/>
     </div>)
 }
 
