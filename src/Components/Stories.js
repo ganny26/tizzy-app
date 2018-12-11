f48933a7c339d001bc5556a4f7971d3536c1fb58
Selvaganesh <selvaganeshrajam@gmail.com>
2018-12-11 19:23:44 +0530

    resolved fixes

--- src/Components/Stories.js
+++ src/Components/Stories.js
@@ -1,48 +1,51 @@
 import React from 'react';
 import _ from 'lodash';
 
-const TOP_STORIES = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty';
-const ITEM_STORIES = (item) => `https://hacker-news.firebaseio.com/v0/item/${item}.json?print=pretty`;
+const TOP_STORIES =
+  'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty';
+const ITEM_STORIES = item =>
+  `https://hacker-news.firebaseio.com/v0/item/${item}.json?print=pretty`;
 
 class Stories extends React.Component {
-
   state = {
     stories: [],
     rd: null
-  }
+  };
 
   async componentWillMount() {
-    let response = await fetch(TOP_STORIES)
-    let result = await response.json()
-    this.setState({
-      stories: _.slice(result, 0, 10)
-    }, () => {
-      this.getStories()
-    })
+    let response = await fetch(TOP_STORIES);
+    let result = await response.json();
+    this.setState(
+      {
+        stories: _.slice(result, 0, 10)
+      },
+      () => {
+        this.getStories();
+      }
+    );
   }
 
   getStories = async () => {
     var items = this.state.stories.map(async (all, item) => {
       let rur = ITEM_STORIES(item);
-      let response = await fetch(rur)
+      let response = await fetch(rur);
       return response.json();
-    })
+    });
     let x = await Promise.all(items);
     this.setState({
       rd: x
-    })
-  }
-
+    });
+  };
 
   render() {
     if (this.state.rd === null) {
-      return <p>Loading...</p>
+      return <p>Loading...</p>;
     } else {
       return this.state.rd.map((v, i) => {
         if (v) {
-          return <h5 key={i}>{v.title}</h5>
+          return <h5 key={i}>{v.title}</h5>;
         }
-      })
+      });
     }
   }
 }
