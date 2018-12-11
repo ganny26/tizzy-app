fb95c66fd7218b9b016cd11f43d73556a1b74d82
Selvaganesh <selvaganeshrajam@gmail.com>
2018-12-11 19:23:17 +0530

    script added for lint

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,20 @@
-const webpack = require('webpack')
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
-const CompressionPlugin = require('compression-webpack-plugin')
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
-  "mode": "development",
-  "entry": {
-    "app": [
-      "@babel/polyfill",
-      "./src/index.js"]
+  mode: 'development',
+  entry: {
+    app: ['@babel/polyfill', './src/index.js']
   },
-  "output": {
+  output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: "[name].[hash].bundle.js",
+    filename: '[name].[hash].bundle.js',
     pathinfo: false
   },
-  "devServer": {
+  devServer: {
     contentBase: path.join(__dirname, 'dist'),
     port: 9000,
     hot: true,
@@ -24,10 +22,10 @@ module.exports = {
     open: true,
     overlay: {
       warnings: true,
-      errors: true,
-    },
+      errors: true
+    }
   },
-  "module": {
+  module: {
     rules: [
       {
         test: /\.js$/,
@@ -36,38 +34,38 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: ['@babel/plugin-proposal-class-properties'],
-          },
-        },
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
       },
       {
         test: /\.scss$/,
         use: [
           {
-            loader: 'style-loader',
+            loader: 'style-loader'
           },
           {
-            loader: 'css-loader',
+            loader: 'css-loader'
           },
           {
             loader: 'sass-loader',
-            options: { modules: true },
-          },
-        ],
-      },
-    ],
+            options: { modules: true }
+          }
+        ]
+      }
+    ]
   },
-  "plugins": [
+  plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new CompressionPlugin(),
     new HtmlWebpackPlugin({
       title: 'Hello',
       template: './src/index.html',
-      filename: 'index.html',
+      filename: 'index.html'
     }),
     new CleanWebpackPlugin(['dist'], {
       root: __dirname,
-      verbose: true,
-    }),
+      verbose: true
+    })
   ]
-}
+};
