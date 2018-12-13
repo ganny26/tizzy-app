ee61aee2c3e63103f20fff8e1ad6f44fc29b8b16
Selvaganesh <selvaganeshrajam@gmail.com>
2018-12-13 16:19:50 +0530

    client changes

---
+++ src/Service/APIClient.js
@@ -0,0 +1,15 @@
+import 'isomorphic-fetch';
+async function fetchDetails(url) {
+  try{
+    let response = await fetch(url);
+    let result = await response.json();
+    return result
+  }catch(e){
+    return null
+  }
+
+}
+
+module.exports = {
+  fetchDetails
+}
