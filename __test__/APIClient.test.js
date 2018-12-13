d713acac8540efb1d66844d113b546eb5ab106fd
Selvaganesh <selvaganeshrajam@gmail.com>
2018-12-13 16:19:01 +0530

    apiclient added

---
+++ __test__/APIClient.test.js
@@ -0,0 +1,32 @@
+import { fetchDetails } from '../src/Service/APIClient';
+import {TOP_STORIES} from '../src/Constant';
+
+describe('run apiclient', () => {
+  beforeEach(() => {
+    console.log('beforeEach called');
+  });
+
+
+  it('return null when no url is present', async () => {
+    expect.assertions(1);
+    const data = await fetchDetails();
+    expect(data).toBeNull()
+  });
+
+  it('return array when url is present', async () => {
+    expect.assertions(2);
+    const data = await fetchDetails(TOP_STORIES);
+    expect(data).not.toBeNull()
+    expect(data.length).toEqual(347)
+  });
+
+  test("returns undefined by default", () => {
+    const mock = jest.fn();
+    let result = mock("foo");
+    expect(result).toBeUndefined();
+    expect(mock).toHaveBeenCalled();
+    expect(mock).toHaveBeenCalledTimes(1);
+    expect(mock).toHaveBeenCalledWith("foo");
+  });
+
+})
