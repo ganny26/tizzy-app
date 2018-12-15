0471b2b66bed91d856d7051aa549ad7a3d602791
Selvaganesh <selvaganeshrajam@gmail.com>
2018-12-15 12:41:12 +0530

    babel hrd added

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './Components/Home';
 import Stories from './Components/Stories';
-
+import Counter from './Components/Counter';
 function App() {
   return (
     <div>
       <Home name="Hacker News List" />
       <Stories />
+      <Counter/>
     </div>
   );
 }
