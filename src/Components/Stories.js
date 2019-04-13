50561c936033698a9353d63331dd7433fb9b82c7
Selvaganesh <selvaganeshrajam@gmail.com>
2019-04-13 16:15:43 +0530

    abort

--- src/Components/Stories.js
+++ src/Components/Stories.js
@@ -3,22 +3,30 @@ import _ from 'lodash'
 import { ITEM_STORIES, TOP_STORIES } from '../Constant'
 
 class Stories extends React.Component {
+  abortController = new AbortController()
   state = {
     stories: [],
     rd: null
   }
 
   async componentDidMount() {
-    let response = await fetch(TOP_STORIES)
-    let result = await response.json()
-    this.setState(
-      {
-        stories: _.slice(result, 0, 10)
-      },
-      () => {
-        this.getStories()
-      }
-    )
+    try {
+      let response = await fetch(TOP_STORIES, {
+        signal: this.abortController.signal
+      })
+      let result = await response.json()
+      this.setState(
+        {
+          stories: _.slice(result, 0, 10)
+        },
+        () => {
+          this.getStories()
+        }
+      )
+    } catch (err) {
+      if (err.name === 'AbortError') return
+      throw error
+    }
   }
 
   getStories = async () => {
@@ -28,9 +36,14 @@ class Stories extends React.Component {
       return response.json()
     })
     let x = await Promise.all(items)
+
     this.setState({
       rd: x
     })
+  }
+
+  componentWillUnmount() {
+    this.abortController.abort()
   }
 
   render() {
